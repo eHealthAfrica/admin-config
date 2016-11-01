4cbcf62fe7820a9b513ea010994879dd1dd6ef67
tabi <tg.twitchett@gmail.com>
2016-11-01 15:50:41 +0100

    Custom eHA functionality
    - Customized src/Utils/ReferenceExtractor.js to add eHA fields to Reference field types
    - Added `lib` folder to branch

--- src/Utils/ReferenceExtractor.js
+++ src/Utils/ReferenceExtractor.js
@@ -1,10 +1,31 @@
+/*
+* A list of field types to instruct ng-admin to treat
+* as Reference fields. This fixes a problem where fields
+* that extended Reference would not be recognised as Reference
+* fields and so would not get their data correctly
+*/
+const REFERENCE_FIELD_TYPES = [
+    'reference',
+    'reference_many',
+    'filteredReference', // custom
+    'addToListReference' // custom
+]
+
+function isReferenceField (field) {
+    return REFERENCE_FIELD_TYPES.includes(field.type())
+}
+
 export default {
 
     getReferencedLists(fields) {
         return this.indexByName(fields.filter(f => f.type() === 'referenced_list'));
     },
     getReferences(fields, withRemoteComplete, optimized = null) {
-        let references = fields.filter(f => f.type() === 'reference' || f.type() === 'reference_many');
+        let references = fields.filter(
+            /* custom  */
+            f => isReferenceField(f)
+            /* custome */
+        );
         if (withRemoteComplete === true) {
             references = references.filter(r => r.remoteComplete());
         } else if (withRemoteComplete === false) {
@@ -28,3 +49,5 @@ export default {
         }, {});
     }
 };
+
+
